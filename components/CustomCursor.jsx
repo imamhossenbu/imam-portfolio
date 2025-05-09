db3d7f6abcf8c613ba4dd7c9af8a252493f555dc
imamhossenbu <imam.cse9.bu@gmail.com>
2025-05-10 00:27:10 +0600

    modify

--- components/CustomCursor.jsx
+++ components/CustomCursor.jsx
@@ -1,4 +1,3 @@
-// components/CustomCursor.jsx
 "use client";
 import { motion, useMotionValue, useSpring } from "framer-motion";
 import { useEffect } from "react";
@@ -12,17 +11,20 @@ const CustomCursor = () => {
     const y = useSpring(cursorY, springConfig);
 
     useEffect(() => {
+        const mediaQuery = window.matchMedia("(min-width: 1024px)");
+        if (!mediaQuery.matches) return;
         const moveCursor = (e) => {
-            cursorX.set(e.clientX - 16); // center offset
+            cursorX.set(e.clientX - 16);
             cursorY.set(e.clientY - 16);
         };
+
         window.addEventListener("mousemove", moveCursor);
         return () => window.removeEventListener("mousemove", moveCursor);
     }, [cursorX, cursorY]);
 
     return (
         <motion.div
-            className="fixed top-0 left-0 w-6 h-6 z-[9999] pointer-events-none rounded-full bg-[#00ff99] mix-blend-difference"
+            className="fixed top-0 left-0 w-6 h-6 z-[9999] pointer-events-none rounded-full bg-[#00ff99] mix-blend-difference hidden lg:block"
             style={{ x, y }}
         />
     );
